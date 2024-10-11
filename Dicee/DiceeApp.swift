ea3789136cab0e1a24947b6bce670459b4262447
szholbarys <sakenzholbarys@gmail.com>
2024-10-11 19:18:42 +0500

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Zholbarys on 11.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
